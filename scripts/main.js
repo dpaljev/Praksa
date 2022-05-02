251c0d5715c1440a0a9e3e8e9eec1c1a1e274b28
dpaljev <p.davidivan@gmail.com>
2022-05-02 16:33:50 +0200

    Zadatak1

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
-window.onload = function() {
-    let myElm = document.createElement("p");
-    myElm.innerText = 'Ovo je dodano naknadno!';
+window.onload = function () {
+    const myElm = document.createElement("p");
+    myElm.innerText = "Ovo je dodano naknadno!";
     document.body.appendChild(myElm);
 }

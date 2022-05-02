2a7cf5649cc64200ead8758e57c84f44093209f4
dpaljev <p.davidivan@gmail.com>
2022-05-02 15:47:05 +0200

    Treci commit (idk how js works yet)

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
-let myElm = document.createElement("p");
-myElm.innerText = 'Ovo je dodano naknadno!';
-document.body.appendChild(myElm);
+window.onload = function() {
+    let myElm = document.createElement("p");
+    myElm.innerText = 'Ovo je dodano naknadno!';
+    document.body.appendChild(myElm);
+}
